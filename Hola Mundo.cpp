88c6094146f02ec5d1cea7fe8816885256737bf2
MauryOG <mauricio1989orellana@gmail.com>
2018-05-14 03:22:32 -0600

    Update Hola Mundo.cpp
    modificacion de primer scanf

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -3,9 +3,15 @@
 
 main()
 {
+	char nombre[50];
 	printf("Hola mundo");
 	puts("Segunda Linea");
 	puts("Tercer Linea");
+	
+	printf("Ingrese su nombre");
+	scanf("%s",&nombre);
+	
+	ptinf
 	
 	getch();
 	return 0;
