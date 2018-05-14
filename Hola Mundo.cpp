9545626cfd9aef0a49b68ae441bfde27738e2267
MauryOG <mauricio1989orellana@gmail.com>
2018-05-14 04:04:43 -0600

    Update Hola Mundo.cpp
    modificado proyecto

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -4,12 +4,8 @@
 main()
 {
 	char nombre[50];
-<<<<<<< HEAD
 	int s;
-=======
 	int a,b,c;
-		
->>>>>>> 1c367a0926e15cd229aae63fb825c518a8d34637
 	printf("Hola mundo");
 	puts("Segunda Linea");
 	puts("Tercer Linea");
@@ -19,16 +15,11 @@ main()
 	
 	printf("Ingrese su nombre");
 	scanf("%s",&nombre);
-	
-<<<<<<< HEAD
+
 	s = a - b;
 	printf("La esta de los dos numeros es: %i",s);
-=======
 	c = a + b;
 	printf("La suma de los dos numeros es: %i",c);
-	
-	ptinf
->>>>>>> 1c367a0926e15cd229aae63fb825c518a8d34637
 	
 	getch();
 	return 0;
